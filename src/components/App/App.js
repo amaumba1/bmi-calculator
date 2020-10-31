048d51db7796ec520ad94d5c7b430377f77588ed
Ally Maumba <amaumba1@gmail.com>
2020-10-31 15:47:50 -0500

    bmi logic

---
+++ src/components/App/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='row center'>
+        <h1 className='white-text'> BMI Tracker </h1>
+      </div>
+      <div className='row'>
+        <div className='col m12 s12'>
+          <div>
+            <div className='row center'>
+              <h4 className='white-text'>7 Day Data</h4>
+            </div>
+            <div className='data-container row'>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
